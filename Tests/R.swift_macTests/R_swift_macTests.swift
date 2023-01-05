4e1a363ed66fdaf12c241b5b378ab5618c82f62b
Obuchi Yuki <yukibochi1@gmail.com>
2023-01-05 21:53:46 +0900

    init

--- Tests/R.swift_macTests/R_swift_macTests.swift
+++ Tests/R.swift_macTests/R_swift_macTests.swift
@@ -1,8 +1,6 @@
 import XCTest
+import RswiftResources
 @testable import R_swift_mac
 
 final class R_swift_macTests: XCTestCase {
-    func testExample() throws {
-//        XCTAssertEqual(R_swift_mac().text, "Hello, World!")
-    }
 }
